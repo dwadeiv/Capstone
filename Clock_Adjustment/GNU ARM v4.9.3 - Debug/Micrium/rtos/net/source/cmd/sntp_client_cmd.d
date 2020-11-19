9d9a1b9b77bb7239e2f5d2a45cf8ac7661d58bdd
David <dawa0697@colorado.edu>
2020-11-19 13:53:35 -0700

    Recovery

--- Clock_Adjustment/GNU ARM v4.9.3 - Debug/Micrium/rtos/net/source/cmd/sntp_client_cmd.d
+++
@@ -1,8 +0,0 @@
-Micrium/rtos/net/source/cmd/sntp_client_cmd.o: \
- ../Micrium/rtos/net/source/cmd/sntp_client_cmd.c \
- /Users/dwadeiv/Documents/Desktop_shit/Capstone/Code/Micrium-OS-Net-Lab-1/App/cfg/rtos_description.h \
- /Users/dwadeiv/Documents/Desktop_shit/Capstone/Code/Micrium-OS-Net-Lab-1/Micrium/rtos/common/include/rtos_opt_def.h
-
-/Users/dwadeiv/Documents/Desktop_shit/Capstone/Code/Micrium-OS-Net-Lab-1/App/cfg/rtos_description.h:
-
-/Users/dwadeiv/Documents/Desktop_shit/Capstone/Code/Micrium-OS-Net-Lab-1/Micrium/rtos/common/include/rtos_opt_def.h:
