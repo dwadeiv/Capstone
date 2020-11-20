2a4273132747d4ed64c07b13dcb88ffa1322ab9e
dante-musto <damu5167@colorado.edu>
2020-11-19 18:07:18 -0700

    Fixed version control

--- Network_Communication_PoC/GNU ARM v4.9.3 - Debug/Micrium/rtos/common/source/clk/clk_cmd.d
+++
@@ -1,8 +0,0 @@
-Micrium/rtos/common/source/clk/clk_cmd.o: \
- ../Micrium/rtos/common/source/clk/clk_cmd.c \
- C:\Users\dante\SimplicityStudio\v4_workspace\Micrium-OS-Net-Lab-1_4\App\cfg/rtos_description.h \
- C:\Users\dante\SimplicityStudio\v4_workspace\Micrium-OS-Net-Lab-1_4\Micrium/rtos/common/include/rtos_opt_def.h
-
-C:\Users\dante\SimplicityStudio\v4_workspace\Micrium-OS-Net-Lab-1_4\App\cfg/rtos_description.h:
-
-C:\Users\dante\SimplicityStudio\v4_workspace\Micrium-OS-Net-Lab-1_4\Micrium/rtos/common/include/rtos_opt_def.h:
--- Network_Communication_PoC/GNU ARM v4.9.3 - Debug/Micrium/rtos/net/source/cmd/net_cmd.d
+++
@@ -1,8 +0,0 @@
-Micrium/rtos/net/source/cmd/net_cmd.o: \
- ../Micrium/rtos/net/source/cmd/net_cmd.c \
- C:\Users\dante\SimplicityStudio\v4_workspace\Micrium-OS-Net-Lab-1_4\App\cfg/rtos_description.h \
- C:\Users\dante\SimplicityStudio\v4_workspace\Micrium-OS-Net-Lab-1_4\Micrium/rtos/common/include/rtos_opt_def.h
-
-C:\Users\dante\SimplicityStudio\v4_workspace\Micrium-OS-Net-Lab-1_4\App\cfg/rtos_description.h:
-
-C:\Users\dante\SimplicityStudio\v4_workspace\Micrium-OS-Net-Lab-1_4\Micrium/rtos/common/include/rtos_opt_def.h:
